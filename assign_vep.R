c6642d0a694d66f8a380e62cae5403a6c06f9b35
SEON-KYEONG JANG <sunkjang@n6458.hoffman2.idre.ucla.edu>
2025-06-01 02:27:09 -0700

    example

---
+++ assign_vep.R
@@ -0,0 +1,118 @@
+library(getopt)
+library(bigsnpr)
+library(data.table)
+library(dplyr)
+
+
+optspec = matrix(c(
+    'gene_list', 'g', 1, 'character', 
+    'id',        'i', 1, 'character', 
+    'vep_path',  'v', 1, 'character', 
+    'out_path',  'o', 1, 'character'),
+    byrow=TRUE, ncol=4)
+
+opt = getopt( optspec)
+#chr = as.integer(opt$chr)
+gene_list = as.character(opt$gene_list) # gene id, genotype path
+vep_path = as.character(opt$vep_path) # Var, Gene, Feature, score
+out_path = as.character(opt$out_path)
+sample.id = as.character(opt$id) # fam file
+
+
+vep.path = paste0("./example/example_score.txt") 
+gene_list = paste0("./example/gene_list")
+sample.id = paste0("./example/example.fam")
+out.path = paste0("./example/example_ukbb_assigned.txt.gz")
+
+### Functions
+assign_vep = function(x){
+    x[is.na(x)]=0
+    res = max( vep.sub.ord$score[which(x>0)] ) # maximum; higher score = higher pathogenicity
+    return(res) # if one carries more than 1 missense variant
+    }
+
+
+### Load VEP scores: make sure that higher scores mean higher pathogenicity
+vep = fread( vep.path ) 
+col.sel = c("Var","Gene","Feature","score")
+vep = vep[, ..col.sel]
+vep = vep[!is.na(score)]
+vep = vep[score!="."]
+vep[,"score":=-as.numeric( score )] # flip sign: high score = higher pathogenicity
+    
+
+### Load gene list : genotype file should have the same individuals
+gene_list = fread(gene_list,header=FALSE) 
+
+### Load sample ID 
+sample.id = fread( sample.id )
+sample.id = sample.id[,"V2"]
+colnames(sample.id)="IID"
+
+### Loop starts
+vep.assigned.all = sample.id
+
+for( i in 1:nrow(gene_list)){
+
+    print(i)
+
+    # Load gene subset
+    gene_id = gene_list$V1[i]
+    tmpfile = tempfile()
+    rds = snp_readBed(gene_list$V2, backingfile = tmpfile)
+    geno = snp_attach(rds)
+    
+    # Subset vep 
+    vep.sub = vep[Gene==gene_id]
+
+    # Subset overlapping vars
+    rvs = geno$map$marker.ID
+    var.ind = which(rvs %in% vep.sub$Var)
+
+    if( length(var.ind)>0){  # run if more than 0 variant is left
+        
+    rds2 = snp_subset( geno, ind.col = var.ind)
+    geno.sub = snp_attach(rds2)
+    sample.id = data.table( geno.sub$fam$sample.ID )
+    colnames(sample.id)="IID"
+
+    rm(geno)
+    rm(rds)
+    gc()
+
+    # Order vep variants
+    rvs2 = geno.sub$map$marker.ID
+    vep.sub.ord = vep.sub[match(rvs2, Var)]
+    
+    # Identify carriers 
+    carr = apply( as.matrix(geno.sub$genotypes[]), 1, function(x) sum(x, na.rm=TRUE)  )
+    carr.ind = which(carr>0)
+    carr.ids = sample.id$IID[carr.ind]
+
+    # Assign VEP scores for carriers     
+    vep.assigned = data.table( apply( as.matrix( geno.sub$genotypes[carr.ind,] ), 1, function(x) assign_vep(x) ) )
+    vep.assigned$IID = carr.ids
+
+    # bind
+    vep.assigned2 = merge(sample.id[,"IID"], vep.assigned, by="IID", all.x=TRUE)
+    colnames(vep.assigned2)[2]=gene_id
+    vep.assigned.all = merge(vep.assigned.all, vep.assigned2, by="IID", all.x=TRUE)
+            
+    } else {
+
+        print("no variants left")
+
+    }
+}
+
+fwrite( vep.assigned.all, file=out.path, row.names=FALSE,quote=FALSE, sep="\t")
+
+
+
+
+
+
+
+
+
+
---
+++ genotype_subset_wrapper.R
@@ -0,0 +1,49 @@
+library(data.table)
+library(getopt)
+
+####### Subset plink geno file by genes: LoF + missense
+
+optspec = matrix(c(
+    'chr', 'c', 1, 'integer', # chromosome name
+    'gs_path','s', 1, 'character', # path for input gene set file
+    'geno_path', 'g', 1, 'character', # path for input exome file (plink prefix)
+    'plink_path', 'p', 1, 'character',
+    'out_path', 'o', 1, 'character'), # output folder path for variant/geno subset
+     byrow=TRUE, ncol=4)
+
+opt = getopt(optspec)
+chr = as.integer(opt$chr)
+gs_path = as.character(opt$gs_path)
+geno_path = as.character(opt$geno_path)
+plink_path = as.character(opt$plink_path)
+out_path = as.character(opt$out_path)
+
+
+
+## gs_path = "./example/ENSG00000205560_gs.txt"
+## plink_path = "/u/project/zaitlenlab/sunkjang/bin/plink2"
+## geno_path =  "./example/example"
+## out_path = "./example/output/"
+
+gs = fread( gs_path )
+gene_list = unique(gs$Gene )
+
+print(chr)
+
+
+for(i in 1:length(gene_list)){
+
+    print(i)
+    gene = gene_list[i]
+    vars = data.table( unique(gs[Gene==gene, Var] ) )
+    var_output = paste0(out_path, gene,"_vars.txt")
+    geno_output = paste0(out_path, gene,"_subset.txt")
+ 
+    fwrite(vars, file=var_output, sep="\t", col.names=FALSE, quote=FALSE)
+    
+    system(paste0(plink_path, " --bfile ", geno_path, " --extract ", var_output," --make-bfile --out ",geno_output)) 
+
+    file.remove(var_output)
+ 
+}
+
